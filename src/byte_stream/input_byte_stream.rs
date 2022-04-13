357a91c901e197ac139e73fa66730ca724933ad1
hyzm <990473625@qq.com>
2022-04-14 01:25:22 +0800

    rename`InputByteStream.read_bytes` to `read` and simplify it

--- src/byte_stream/input_byte_stream.rs
+++ src/byte_stream/input_byte_stream.rs
@@ -1,4 +1,4 @@
-use std::mem::transmute;
+use std::mem::{size_of, transmute};
 
 use crate::byte_stream::{Endianness, get_platform_endianness};
 use crate::byte_stream::swap_bytes::*;
@@ -18,7 +18,8 @@ impl<'a> InputByteStream<'a> {
         }
     }
 
-    fn read_bytes<T: Sized>(&mut self, num_bytes: usize) -> T {
+    fn read<T: Sized>(&mut self) -> T {
+        let num_bytes = size_of::<T>();
         let bytes = &self.buf[self.head..self.head + num_bytes];
         self.head += num_bytes;
         unsafe {
@@ -26,8 +27,8 @@ impl<'a> InputByteStream<'a> {
         }
     }
 
-    pub fn read_u8(&mut self) -> u8 { self.read_bytes(1) }
-    pub fn read_i8(&mut self) -> i8 { self.read_bytes(1) }
+    pub fn read_u8(&mut self) -> u8 { self.read() }
+    pub fn read_i8(&mut self) -> i8 { self.read() }
 
     pub fn read_bool(&mut self) -> bool {
         let byte = self.read_u8();
@@ -35,7 +36,7 @@ impl<'a> InputByteStream<'a> {
     }
 
     pub fn read_u16(&mut self) -> u16 {
-        let data = self.read_bytes(2);
+        let data = self.read();
         if self.endianness != get_platform_endianness() {
             swap_2_bytes(data)
         } else {
@@ -45,7 +46,7 @@ impl<'a> InputByteStream<'a> {
     pub fn read_i16(&mut self) -> i16 { self.read_u16() as i16 }
 
     pub fn read_u32(&mut self) -> u32 {
-        let data = self.read_bytes(4);
+        let data = self.read();
         if self.endianness != get_platform_endianness() {
             swap_4_bytes(data)
         } else {
@@ -55,7 +56,7 @@ impl<'a> InputByteStream<'a> {
     pub fn read_i32(&mut self) -> i32 { self.read_u32() as i32 }
 
     pub fn read_u64(&mut self) -> u64 {
-        let data = self.read_bytes(8);
+        let data = self.read();
         if self.endianness != get_platform_endianness() {
             swap_8_bytes(data)
         } else {
